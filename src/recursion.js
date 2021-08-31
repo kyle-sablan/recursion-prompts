62f73d9f2edb362a867860ebb74e694c4a9fa891
kyle_sablan <kyle.sablan@gmail.com>
2021-08-31 15:21:46 -0700

    Completed first 4 problems on recursion (If you get stuck in the future, map out exectuion contexts step by step)

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,55 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if(n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (!Array.isArray(array[0])) {
+    return array[0] + arraySum(array.slice(1));
+  }
+  return arraySum(array[0]) + arraySum(array.slice(1));
 };
+
+
+
+
+
+
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  }
+  if (n < 0) {
+    return isEven(n + 2);
+  } else {
+    return isEven(n - 2)
+  }
 };
 
 // 5. Sum all integers below a given integer.
@@ -32,6 +67,9 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y) {
+    return [];
+  }
 };
 
 // 7. Compute the exponent of a number.
